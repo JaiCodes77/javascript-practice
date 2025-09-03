14ef09c4a00229a6dd39ebad5265e8252a8766b6
Jai Pandey <jaipandeyzerozero@gmail.com>
2025-09-03 11:13:01 +0530

    exp tracker 2

--- expensetracker2/index.js
+++ expensetracker2/index.js
@@ -1,50 +1,61 @@
-const expenses = [{description : 'lunch', amount : 2000, category : 'food'},
-    {description : 'mobile', amount : 10000, category : 'Electronics'},
-    {description : 'shoes', amount : 2500, category : 'Clothing'}]; 
+const expenses = [
+  { description: "lunch", amount: 2000, category: "food" },
+  { description: "mobile", amount: 10000, category: "Electronics" },
+  { description: "shoes", amount: 2500, category: "Clothing" },
+];
 
-    const descInput = document.getElementById('descInp');
-    const categoryInput = document.getElementById('categoryInp');
-    const amountInput = document.getElementById('amountInp');
-    const display = document.getElementById('display');
-    const addExpBtn = document.getElementById('addExpBtn');
-    const showtotal = document.getElementById('showtotal');
+const descInput = document.getElementById("descInp");
+const categoryInput = document.getElementById("categoryInp");
+const amountInput = document.getElementById("amountInp");
+const display = document.getElementById("display");
+const addExpBtn = document.getElementById("addExpBtn");
+const showtotal = document.getElementById("showtotal");
+const dropdown = document.getElementById("dropdownBtn");
 
-    function renderExpenses(){
-        display.innerHTML = '';
+function renderExpenses(filterCategory = "all") {
+  display.innerHTML = "";
+  showtotal.innerHTML = "";
 
-        expenses.forEach((exp)=>{
-            let divDesc = document.createElement('div');
-            divDesc.classList.add('div');
-            divDesc.innerHTML = `${exp.description}-${exp.category}`
+  let filteredExpense =
+    filterCategory === "all"
+      ? expenses
+      : expenses.filter((exp) => exp.category === filterCategory);
 
-            let divAmt = document.createElement('div');
-            divAmt.classList.add('div');
-            divAmt.innerHTML = `${exp.amount}`
+  filteredExpense.forEach((exp) => {
+    let expenseDiv = document.createElement("div");
+    let divDesc = document.createElement("div");
+    divDesc.innerHTML = `${exp.description}-${exp.category}`;
+    let divAmt = document.createElement("div");
+    divAmt.innerHTML = `${exp.amount}`;
 
-             display.appendChild(divDesc)
-            display.appendChild(divAmt) 
+    expenseDiv.appendChild(divDesc);
+    expenseDiv.appendChild(divAmt);
+    display.appendChild(expenseDiv);
+  });
 
-            let total = expenses.reduce((acc,exp)=>acc+Number(exp.amount),0)
-            let displaytotal = document.createElement('div');
-            displaytotal.innerHTML = `${total}`;
+  let total = filteredExpense.reduce((acc, exp) => acc + Number(exp.amount), 0);
+  let displayTotal = document.createElement("div");
+  displayTotal.innerHTML = `Total : Rs-${total}`;
+  showtotal.appendChild(displayTotal);
+}
 
-            showtotal.appendChild(displaytotal);
-            
-        })
-    }  
+addExpBtn.addEventListener("click", () => {
+  let newExpense = {
+    description: descInput.value,
+    category: categoryInput.value,
+    amount: amountInput.value,
+  };
 
-    addExpBtn.addEventListener('click',()=>{
-        let newExpense = {
-            description : descInput.value,
-            category : categoryInput.value,
-            amount : amountInput.value
-        }
+  expenses.push(newExpense);
 
-        expenses.push(newExpense);
-        renderExpenses();
-    }) 
+  descInput.value = "";
+  categoryInput.value = "";
+  amountInput.value = "";
+  renderExpenses(dropdown.value);
+});
 
+dropdown.addEventListener("change", () => {
+  renderExpenses(dropdown.value);
+});
 
-
-    renderExpenses(); 
-
+renderExpenses();
